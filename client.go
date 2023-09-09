ef032163050745de4d1d93691bbf7746292a8ac7
Thomas Lo <thomasjinlo@gmail.com>
2023-09-09 10:23:59 -0700

    chat server and client barely operational local only

--- client.go
+++ client.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
-    "fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
-
 
 func main() {
     conn, err := net.Dial("tcp4", "127.0.0.1:5000")
@@ -13,6 +16,60 @@ func main() {
         log.Fatal(err)
     }
     defer conn.Close()
-
     fmt.Println("Connection to server!")
+
+    closeCh := CloseChannel(make(chan struct{}))
+    go handleInterrupt(closeCh)
+    go handleServerShutdown(conn, closeCh)
+    go handleWrites(conn)
+
+    <-closeCh
+}
+
+func handleWrites(conn net.Conn) {
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        message, err := reader.ReadString('\n')
+        if err != nil {
+            log.Fatal("Could not read string")
+            return
+        }
+
+        _, err = conn.Write([]byte(message))
+        if err != nil {
+            fmt.Println("error writing to connection", err)
+            return
+        }
+    }
+}
+
+type CloseChannel chan struct{}
+
+func handleInterrupt(closeCh CloseChannel) {
+    sigCh := make(chan os.Signal, 1)
+    signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+    <-sigCh
+    close(closeCh)
+}
+
+func handleServerShutdown(conn net.Conn, closeCh CloseChannel) {
+    reader := bufio.NewReader(conn)
+    for {
+        _, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Println("Server closed the connection:", err)
+            // check for channel closure. Channel will already be closed when
+            // forcefully interrupted.
+            select {
+            case _, ok := <-closeCh:
+                if ok {
+                    close(closeCh)
+                }
+            default:
+                close(closeCh)
+            }
+            return
+        }
+    }
 }
--- server.go
+++ server.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 )
 
@@ -16,14 +15,7 @@ const (
     CONN_TYPE string = "tcp4"
 )
 
-func handleConnection(conn net.Conn, wg *sync.WaitGroup, done chan bool) {
-    defer wg.Done()
-    defer conn.Close()
-
-    fmt.Println("Connected to client!", conn.RemoteAddr().String())
-    <-done
-}
-
+type CloseChannel chan struct{}
 
 func main() {
     ADDR := CONN_HOST + ":" + CONN_PORT
@@ -31,30 +23,54 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-
     defer listener.Close()
-
     fmt.Println("Waiting for incoming connections...")
 
-    done := make(chan bool)
-    var wg sync.WaitGroup
+    closeCh := make(CloseChannel)
+    connCh := make(chan net.Conn, 4)
+    go handleInterrupt(closeCh, connCh)
+    go acceptConnections(listener, connCh)
 
+    <-closeCh
+}
+
+func handleInterrupt(closeCh CloseChannel, connCh chan net.Conn) {
     sigCh := make(chan os.Signal, 1)
     signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
-    go func() {
-        <-sigCh
-        fmt.Println("\nShutting down...")
-        listener.Close()
-    }()
 
+    <-sigCh
+    go func() {
+        for conn := range connCh {
+            conn.Close()
+        }
+    }()
+    close(connCh)
+    close(closeCh)
+}
+
+func acceptConnections(listener net.Listener, connCh chan net.Conn) {
     for {
         conn, err := listener.Accept()
         if err != nil {
             log.Fatal(err)
         }
-        wg.Add(1)
-        go handleConnection(conn, &wg, done)
+        connCh <- conn
+        go handleConnection(conn)
     }
+}
 
-    wg.Wait()
+func handleConnection(conn net.Conn) {
+    fmt.Println("Accepted connection:", conn.RemoteAddr().String())
+
+    for {
+        buf := make([]byte, 2048)
+        n, err := conn.Read(buf)
+        if err != nil {
+            fmt.Println("Closing connection to:", conn.RemoteAddr().String())
+            conn.Close()
+            return
+        }
+        message := buf[:n]
+        fmt.Println(string(message))
+    }
 }
